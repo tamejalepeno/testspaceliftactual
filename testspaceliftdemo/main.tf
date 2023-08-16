1990c2b2a5a001a86652e60aa65be5f1b5e32750
Bollinger_Christopher_cbollinger7 <cbollinger7@gatech.edu>
2023-08-16 14:35:00 -0400

    blah

---
+++ testspaceliftdemo/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  assume_role_with_web_identity {
+    role_arn                = "arn:aws:iam::1234567890:role/spacelift-role"
+    web_identity_token_file = "/mnt/workspace/spacelift.oidc"
+  }
+
+  default_tags {
+    tags = {
+      Environment = "Sandbox"
+      Terraform   = "True"
+      Repo        = "spacelift-prep"
+      Project     = "Spacelift tutorial"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = thisismytestbucketforspaceliftlskjdfoe8983kd
+}
+
