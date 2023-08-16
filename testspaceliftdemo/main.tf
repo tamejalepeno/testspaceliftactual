7bd9d1b3eef02cfcc6974cdc1b6253677ef3ff88
Bollinger_Christopher_cbollinger7 <cbollinger7@gatech.edu>
2023-08-16 16:16:00 -0400

    moved

--- testspaceliftdemo/main.tf
+++ testspaceliftdemo/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::671866886773:role/spacelift-role"
     web_identity_token_file = "/mnt/workspace/spacelift.oidc"
+    region                  = "us-east-1"
   }
 
   default_tags {
--- testspaceliftdemo/providers.tf
+++ testspaceliftdemo/providers.tf
@@ -4,9 +4,8 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       #version = "~> 4.0"
-      region  = "us-east-1"
     }
   }
 }
